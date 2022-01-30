09a0f11281228f33e05373cec6a27555b594c459
zoredeaxx <87483466+zoredeaxx@users.noreply.github.com>
2022-01-30 14:30:47 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,15 +1,35 @@
+import platform
 import traceback
 import json
 import sys
 import os
+import wget
+import logging
+from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
+
+log = logging.getLogger(__name__)
+
+CONFIG_FILE_URL = os.environ.get("CONFIG_FILE_URL", None)
+
+if CONFIG_FILE_URL:
+    if os.path.exists(".env"):
+        os.remove(".env")
+    wget.download(CONFIG_FILE_URL, out=".env")
+    print("\n")
+    load_dotenv()
 
 try:
     port = int(os.environ.get("PORT", "8080"))
-except ValueError:
+except Exception as e:
+    print(e)
     port = -1
 if not 1 <= port <= 65535:
-    print("Please make sure the PORT environment variable is an integer between 1 and 65535")
+    print(
+        "Please make sure the PORT environment variable is an integer between 1 and 65535"
+    )
     sys.exit(1)
 
 try:
@@ -22,26 +42,8 @@ except (KeyError, ValueError):
     sys.exit(1)
 
 try:
-    # index_settings_str = os.environ["INDEX_SETTINGS"].strip()
-
-    # index_settings = json.loads(index_settings_str)
-
-    index_settings = {
-      "index_all": False,
-      "index_private":True,
-      "index_group": True,
-      "index_channel": True,
-      "exclude_chats": [],
-      "include_chats": [int(os.environ["INDEXING_CHAT"])],#my index chat
-      "otg": {
-          "enable": True,
-          "include_private": True,
-          "include_group": True,
-          "include_channel": True
-      }
-    }
-    otg_settings = index_settings['otg']
-    enable_otg = otg_settings['enable']
+    #index_settings_str = os.environ["INDEX_SETTINGS"].strip()
+    #index_settings = json.loads(index_settings_str)
 except:
     traceback.print_exc()
     print("\n\nPlease set the INDEX_SETTINGS environment variable correctly")
@@ -54,16 +56,26 @@ except (KeyError, ValueError):
     print("\n\nPlease set the SESSION_STRING environment variable correctly")
     sys.exit(1)
 
-# try:
-#     bot_token = os.environ["BOT_TOKEN"]
-# except (KeyError, ValueError):
-#     traceback.print_exc()
-#     print("\n\nPlease set the BOT_TOKEN environment variable correctly")
-#     sys.exit(1)
-
-
-
 host = os.environ.get("HOST", "0.0.0.0")
 debug = bool(os.environ.get("DEBUG"))
-chat_ids = []
-alias_ids = []
+block_downloads = bool(os.environ.get("BLOCK_DOWNLOADS"))
+results_per_page = int(os.environ.get("RESULTS_PER_PAGE", "20"))
+logo_folder = Path("./temp/logo/" if platform.system() == "Windows" else "/tmp/logo")
+if not logo_folder.exists():
+    logo_folder.mkdir(parents=True)
+username = os.environ.get("TGINDEX_USERNAME", "")
+password = os.environ.get("PASSWORD", "")
+SHORT_URL_LEN = int(os.environ.get("SHORT_URL_LEN", 3))
+authenticated = bool(username and password)
+SESSION_COOKIE_LIFETIME = int(os.environ.get("SESSION_COOKIE_LIFETIME") or "60")
+try:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    if len(SECRET_KEY) != 32:
+        raise ValueError("SECRET_KEY should be exactly 32 charaters long")
+except (KeyError, ValueError):
+    if authenticated:
+        traceback.print_exc()
+        print("\n\nPlease set the SECRET_KEY environment variable correctly")
+        sys.exit(1)
+    else:
+        SECRET_KEY = ""
